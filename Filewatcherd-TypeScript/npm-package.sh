a89adcb7d97973ea2fb97f9389b9bdfbe105b6c1
Tim Etchells <timetchells@ibm.com>
2019-06-17 12:54:39 -0400

    Add tar step to npm package
    Signed-off-by: Tim Etchells <timetchells@ibm.com>

--- Filewatcherd-TypeScript/npm-package.sh
+++ Filewatcherd-TypeScript/npm-package.sh
@@ -8,7 +8,15 @@ libDir="${outDir}/lib"
 set -ex
 
 npm run tslint
-rm -rf $libDir $decDir
+rm -rf $libDir
+npm ci
 tsc -d --outDir $outDir --sourceMap false
 
 cp -v package.json package-lock.json $outDir
+
+version=$(node -e "console.log(require('./package.json').version);")
+# version="1.0.0"
+
+tarball=filewatcherd-node_${version}.tar.gz
+tar -zcf $tarball $outDir
+# echo $tarball
